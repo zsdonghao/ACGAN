13c027f98db5670264b38f8e18a9de24197f1000
zsdonghao <dhsig552@163.com>
2017-12-07 15:49:53 +0000

    1st commit

---
+++ main.py
@@ -0,0 +1,118 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+import tensorflow as tf
+import tensorlayer as tl
+import model, time, os
+import numpy as np
+
+save_dir = "samples/experiment"
+tl.files.exists_or_mkdir(save_dir)
+checkpoint_dir = "checkpoint/experiment"
+tl.files.exists_or_mkdir(checkpoint_dir)
+save_dir_d = os.path.join(checkpoint_dir, 'd.npz')
+save_dir_g = os.path.join(checkpoint_dir, 'g.npz')
+
+def _data_aug_fn(im):
+    im = tl.prepro.flip_axis(im, axis=1, is_random=True)
+    im = im / 127.5 - 1
+    return im
+
+def main():
+    X_train, y_train, X_test, y_test = tl.files.load_cifar10_dataset(shape=(-1, 32, 32, 3))
+
+    batch_size = 64
+    z_dim = 128
+    t_image = tf.placeholder(tf.float32, [None, 32, 32, 3], 'image')
+    t_z     = tf.placeholder(tf.float32, [None, z_dim]    , 'noise')
+    t_class = tf.placeholder(tf.int64  , [None, ]        , 'class')
+
+    net_g = model.G(t_z, t_class, is_train=True, reuse=False, batch_size=batch_size)
+    net_d, d_fake, d_fake_class = model.D(net_g.outputs, is_train=True, reuse=False)
+    net_d, d_real, d_real_class = model.D(t_image      , is_train=True, reuse=True)
+
+    # for testing
+    net_g_test = model.G(t_z, t_class, is_train=False, reuse=True, batch_size=100)
+
+    # net_g.print_layers()
+    # net_d.print_layers()
+    # exit()
+
+    # class
+    ce_real = tl.cost.cross_entropy(d_real_class, t_class, name='d_real_class')
+    ce_fake = tl.cost.cross_entropy(d_fake_class, t_class, name='d_fake_class')
+    q_loss = ce_real + ce_fake
+
+    # DC-GAN
+    d_loss1 = tl.cost.sigmoid_cross_entropy(d_real, tf.ones_like(d_real), name='d_real')
+    d_loss2 = tl.cost.sigmoid_cross_entropy(d_fake, tf.zeros_like(d_fake), name='d_fake')
+    g_loss1 = tl.cost.sigmoid_cross_entropy(d_fake, tf.ones_like(d_fake), name='g_fake')
+    # LS-GAN
+    # d_loss1 = tl.cost.mean_squared_error(d_real, tf.ones_like(d_real), is_mean=True, name='d_real')
+    # d_loss2 = tl.cost.mean_squared_error(d_fake, tf.zeros_like(d_fake), is_mean=True, name='d_fake')
+    # g_loss1 = tl.cost.mean_squared_error(d_fake, tf.ones_like(d_fake), is_mean=True, name='g_fake')
+
+    d_loss = d_loss1 + d_loss2 + ce_real #- ce_fake
+    g_loss = g_loss1 + ce_fake
+
+    lr = 0.0002
+    lr_decay = 0.5
+    n_epoch = 50
+    decay_every = 10
+    beta1 = 0.5
+
+    d_vars = tl.layers.get_variables_with_name('discriminator', True, True)
+    g_vars = tl.layers.get_variables_with_name('generator', True, True)
+
+    with tf.variable_scope('learning_rate'):
+        lr_v = tf.Variable(lr, trainable=False)
+    d_optim = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(d_loss, var_list=d_vars)
+    g_optim = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(g_loss, var_list=g_vars)
+
+    sess = tf.InteractiveSession()
+    tl.layers.initialize_global_variables(sess)
+
+    # tl.files.load_and_assign_npz(sess, save_dir_d, net_d)
+    # tl.files.load_and_assign_npz(sess, save_dir_d, net_g)
+
+    print_freq = 5
+    n_step_epoch = int(len(y_train) / batch_size)
+    sample_z = np.random.normal(loc=0.0, scale=1.0, size=(100, z_dim))
+    sample_c = [i for i in range(10)] * 10      #[[i]*10 for i in range(10)]
+    # print(len(sample_c), sample_z.shape)
+    # exit()
+    for epoch in range(n_epoch):
+        epoch_time = time.time()
+        step, total_D, total_G = 0, 0, 0
+        for b_x, b_y in tl.iterate.minibatches(X_train, y_train, batch_size, shuffle=True):
+            step_time = time.time()
+            b_z = np.random.normal(loc=0.0, scale=1.0, size=(batch_size, z_dim))
+            b_x = tl.prepro.threading_data(b_x, _data_aug_fn)
+            # update D
+            errD, _ = sess.run([d_loss, d_optim], {t_image: b_x, t_class: b_y, t_z: b_z})
+            # update G
+            errG, _ = sess.run([g_loss, g_optim], {t_image: b_x, t_class: b_y, t_z: b_z})
+            print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                % (epoch, n_epoch, step, n_step_epoch, time.time() - step_time, errD, errG))
+            step += 1; total_D += errD ; total_G += errG
+        print("[*] Epoch [%2d/%2d] time: %4.4f, avg_d_loss: %.8f, avg_g_loss: %.8f" %
+                (epoch, n_epoch, time.time()-epoch_time, total_D/step, total_G/step))
+
+        out = sess.run(net_g_test.outputs, {t_z: sample_z, t_class: sample_c})        # ni = int(np.ceil(np.sqrt(batch_size)))
+        tl.vis.save_images(out, [10, 10], save_dir + '/train_{:02d}c.png'.format(epoch))
+
+        ## save model
+        if (epoch != 0) and (epoch % 5) == 0:
+            tl.files.save_npz(net_d.all_params, name=save_dir_d, sess=sess)
+            tl.files.save_npz(net_g.all_params, name=save_dir_g, sess=sess)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mode', type=str, default="train",
+                       help='train, test')
+
+    args = parser.parse_args()
+
+    if args.mode == "train":
+        main()
---
+++ model.py
@@ -0,0 +1,67 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+import tensorflow as tf
+import tensorlayer as tl
+from tensorlayer.layers import *
+
+def G(z, c, is_train=False, reuse=False, batch_size=64):                # ref: https://github.com/zsdonghao/dcgan/blob/master/model.py
+    image_size = 32
+    s2, s4, s8, s16 = int(image_size/2), int(image_size/4), int(image_size/8), int(image_size/16)
+    gf_dim = 64
+    lrelu = lambda x : tl.act.lrelu(x, 0.2)
+    with tf.variable_scope("generator", reuse=reuse):
+        tl.layers.set_name_reuse(reuse)
+
+        nz = InputLayer(z, name='inz')
+        nc = EmbeddingInputlayer(c,
+                vocabulary_size = 10,
+                embedding_size = 10,
+                name='inc')
+        nc = FlattenLayer(nc, 'flat')
+        n = ConcatLayer([nz, nc], 1, name='concat')
+
+        n = DenseLayer(n, n_units=gf_dim*8*s8*s8, name='d1')
+        n = ReshapeLayer(n, [-1, s8, s8, gf_dim*8], name='reshape')
+        n = BatchNormLayer(n, act=lrelu, is_train=is_train, name='bn1')
+        print(n.outputs)
+        exit()
+
+        n = DeConv2d(n, gf_dim*2, (3, 3), out_size=(s4, s4), strides=(2, 2),
+                batch_size=batch_size, padding='SAME', name='de1')
+        n = BatchNormLayer(n, act=lrelu, is_train=is_train, name='dbn1')
+
+        n = DeConv2d(n, gf_dim, (3, 3), out_size=(s2, s2), strides=(2, 2),
+                batch_size=batch_size, padding='SAME', name='de2')
+        n = BatchNormLayer(n, act=lrelu, is_train=is_train, name='dbn2')
+
+        n = DeConv2d(n, 3, (3, 3), out_size=(image_size, image_size),
+                batch_size=batch_size, strides=(2, 2), act=tf.nn.tanh, padding='SAME', name='do')
+        return n
+
+def D(x, is_train=False, reuse=False):
+    image_size = 32
+    s2, s4, s8, s16 = int(image_size/2), int(image_size/4), int(image_size/8), int(image_size/16)
+    df_dim = 64
+    lrelu = lambda x : tl.act.lrelu(x, 0.2)
+    with tf.variable_scope("discriminator", reuse=reuse):
+        tl.layers.set_name_reuse(reuse)
+
+        n = InputLayer(x, name='in')
+        n = Conv2d(n, df_dim, (3, 3), (2, 2), act=lrelu, padding='SAME', name='c0')
+
+        n = Conv2d(n, df_dim*2, (3, 3), (2, 2), padding='SAME', name='c1')
+        n = BatchNormLayer(n, act=lrelu, is_train=is_train, name='bn1')
+
+        n = Conv2d(n, df_dim*4, (3, 3), (2, 2), padding='SAME', name='c2')
+        n = BatchNormLayer(n, act=lrelu, is_train=is_train, name='bn2')
+
+        # n = Conv2d(n, df_dim*8, (5, 5), (2, 2), act=None,
+        #         padding='SAME', W_init=w_init, name='d/h3/conv2d')
+        # n = BatchNormLayer(n, act=lambda x: tl.act.lrelu(x, 0.2),
+        #         is_train=is_train, gamma_init=gamma_init, name='d/h3/batch_norm')
+
+        n = FlattenLayer(n, name='flat')
+        n1 = DenseLayer(n, n_units=1 , name='real/fake')
+        n2 = DenseLayer(n, n_units=10, name='class')
+        n1 = tl.layers.merge_networks([n1, n2])
+    return n1, n1.outputs, n2.outputs
